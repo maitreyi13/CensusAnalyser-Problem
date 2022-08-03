7a70816b526a27da219e000103d840f962d2878f
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-08-03 12:36:22 +0530

    Welcome to Indian States Census Analyser Problem

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("***** Indian States Census Analyser Problem *****");
+    }
+}
